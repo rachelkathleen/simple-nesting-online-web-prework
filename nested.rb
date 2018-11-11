78e30c4fae3082b7b3234ed4f71f468674a9e4d2
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-11 21:05:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,7 +66,7 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{
+  {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,6 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash(:yukihiro_matsumoto) = {}
 
 
 end
